20a3853d0e2d5ab7d538be7fcbb518a0be25c449
Beren Erchamion <beren@houseofbeor.net>
2018-03-10 14:53:15 -0500

    Getting the add form working...

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -6,16 +6,28 @@ import { Link } from 'react-router-dom'
 
 class AddPost extends Component{
   render(){
+    const { categories } = this.props
     return(
-      <div className="addpost">
-      Hey there - add something...
+      <div className="container">
+        <div className="categories">
+          Hey there - add something... from one of these categories:
+          <ul className="category-list">
+            {categories.map((category) => (
+              <li>{category.name}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="add-post-form">
+          This is where the form will go.
+        </div>
       </div>
     )
   }
 }
 
-function mapStateToProps({ posts }){
+function mapStateToProps({ categories }){
   return {
+    categories: categories
 
   }
 }
