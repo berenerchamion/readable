22b82d7081302d4c434e227bba1e7af4ac850964
Beren Erchamion <beren@houseofbeor.net>
2018-03-11 17:22:21 -0400

    starting on the edit post feature.

---
+++ src/components/EditPost.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux'
+import * as actions from '../actions'
+import { Link } from 'react-router-dom'
+
+class EditPost extends Component{
+
+}
